0749739e27d4706f662419d481af207c26c4d85d
MarlonMorais <glaucianerod@hotmail.com>
2017-05-28 13:06:27 -0300

    Create exemplo10.php

---
+++ exemplo10.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>Exemplo 10</title>
+<meta charset="utf-8">
+</head>
+   <body>
+      <h1>estrutura de repetição(while)</h1>
+   <?php
+      $num=1;
+      while ($num <= 10) {
+      echo "mostrar numero: $num <br>";
+      $num++;
+      }
+   ?>
+  </body>
+</html>
