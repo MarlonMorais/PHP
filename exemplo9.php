7bfed7d541f42f2351bf5826887030e78a5ab0fb
MarlonMorais <glaucianerod@hotmail.com>
2017-05-28 13:00:58 -0300

    Create exemplo9.php

---
+++ exemplo9.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Exemplo 9</title>	
+<meta charset="utf-8">
+</head>
+  <body>
+    <h1>estrutura de repetição(for)</h1>
+   <?php
+      for ($i=1; $i <=10 ; $i++) { 
+       echo "<b> número: $i<br>";
+      }
+   ?>
+  </body>
+</html>
